c4257f1020465ad8656410a45eb8140d19b78ab3
David Zheng <davidzheng@knights.ucf.edu>
2019-10-12 23:12:34 -0400

    mobile

--- main.js
+++ main.js
@@ -6,8 +6,3 @@ function closevid() {
   document.getElementById("video1").src = "";
   document.getElementById("video-container").style.display = "none";
 }
-
-window.addEventListener('resize', () => {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
