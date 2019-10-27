b4ea9c6cb8f0b79e579377b4c6f6ceb033736627
David Zheng <davidzheng@knights.ucf.edu>
2019-10-26 22:50:44 -0400

    photo optimization

--- main.js
+++ main.js
@@ -6,14 +6,3 @@ function closevid() {
   document.getElementById("video1").src = "";
   document.getElementById("video-container").style.display = "none";
 }
-
-function playvideo(div) {
-  // var div = document.getElementById(this.id);
-  console.log(div.id);
-  var height = div.offsetHeight;
-  var width = div.offsetWidth;
-  div.style.display = 'none';
-  div.nextElementSibling.height = height;
-  div.nextElementSibling.width = width;
-  div.nextElementSibling.style.display = 'block';
-}
